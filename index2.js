92dcc806172b83c540850fa0f7c58205edb0cdc0
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-31 00:07:33 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,14 @@
 // write document in console you will get the html web page which you have open in we browser
+typeof document
+//'object'
+document.querySelector('p')// it will return the top most paragraph
+document.querySelectorAll('p')// it will return all paragraph
+document.getElementById('h1');
+document.getElementsByClassName('paragragh21');
+// event handling 
+const target= document.querySelector('body')
+function onClick()
+{
+  console.log('click the body')
+}
+target.addEventListener('click', onClick) // no of times you click on web page it will count
